f3e38c97bb8cade6d7836a6874d19113ec72be19
kydyrsha <sanatkydyrsha@gmail.com>
2021-03-16 16:31:52 +0600

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    const handlerClick = (e) => {
+        console.log(1212, e)
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target)
+    }
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handlerClick}>click me</button>
+            <button onClick={(e) => handleClickAgain('yasha', e)}>click me again</button>
         </div>
     )
 }
